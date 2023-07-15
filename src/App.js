41e11ffac29a2a542330e2c1b4ff72fc2584440a
Shifana nasrin <shifananazrin15@gmail.com>
2023-07-15 17:02:22 +0530

    erro fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {getCurrentLocation } from 'current-location-geo'
+import Autosuggest from 'react-autosuggest';
+import './App.css';
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
   const [unit, setUnit] = useState('metric'); // default unit is Celsius (°C)
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          fetchLocationName(latitude, longitude);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-    } else {
-      console.error('Geolocation is not supported by this browser.');
-    }
+    getCurrentLocation(function (err, position) {
+      if (err) {
+        console.error('Error:', err);
+      } else {
+        fetchLocationName(position.latitude,position.longitude);
+        console.log('Latitude:', position.latitude);
+        console.log('Longitude:', position.longitude);
+      }
+    });
   }, []);
 
   const fetchLocationName = async (latitude, longitude) => {
@@ -35,19 +37,18 @@ function App() {
     }
   };
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=895284fb2d2c50a520ea537456963d9c`)
-        .then((response) => {
-          setData(response.data);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      setLocation('');
-    }
+  const searchLocation = () => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=${unit}&appid=895284fb2d2c50a520ea537456963d9c`)
+      .then((response) => {
+        setData(response.data);
+        setLocation(response.data.name);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    setInputValue('');
   };
 
   const toggleUnit = () => {
@@ -62,24 +63,72 @@ function App() {
     }
   };
 
+  const fetchCountrySuggestions = async (value) => {
+    try {
+      const response = await axios.get('https://restcountries.com/v3.1/all');
+      const countries = response.data;
+      const countryNames = countries.map((country) => country.name.common);
+      const inputValue = value.trim().toLowerCase();
+      const inputLength = inputValue.length;
+      const suggestions = inputLength === 0 ? [] : countryNames.filter((country) => country.toLowerCase().slice(0, inputLength) === inputValue);
+      setSuggestions(suggestions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    fetchCountrySuggestions(value);
+  };
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const onChange = (event, { newValue }) => {
+    setInputValue(newValue);
+  };
+
+  const renderSuggestion = (suggestion) => {
+    return (
+      <div className="suggestion">
+        {suggestion}
+      </div>
+    );
+  };
+
+  const inputProps = {
+    placeholder: 'Enter Location',
+    value: inputValue,
+    onChange: onChange,
+    onKeyPress: (event) => {
+      if (event.key === 'Enter') {
+        searchLocation();
+      }
+    }
+  };
 
   return (
     <div className="app">
-      <div className="search">
-        <input
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-        />
-      </div>
+   <div className="search">
+
+    <Autosuggest
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+      onSuggestionsClearRequested={onSuggestionsClearRequested}
+      getSuggestionValue={(suggestion) => suggestion}
+      renderSuggestion={renderSuggestion}
+      inputProps={inputProps}
+    />
+  
+</div>
+
       <div className="container">
         <div className="top">
           <div className="location">
             <p>{location}</p>
           </div>
-           <div className="temp">
+          <div className="temp">
             {data.main ? (
               <>
                 <h1>{convertTemperature(data.main.temp).toFixed()}°{unit === 'imperial' ? 'F' : 'C'}</h1>
@@ -102,17 +151,13 @@ function App() {
               {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
+
             
-            {/* <div className="City">
-              {data.main ? <p className="bold">{data.name}</p> : null}
-              <p>City</p>
-            </div> */}
-           
-           <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
+
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed.toFixed()}MPH</p> : null}
               <p> Wind Speed</p>
             </div>
-           
           </div>
         )}
       </div>
@@ -121,3 +166,4 @@ function App() {
 }
 
 export default App;
+
